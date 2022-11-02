e75dc3a1305b8cd2e459cbe6c5d7004ed6416c78
Ajinkya Indulkar <26824103+AjinkyaIndulkar@users.noreply.github.com>
2022-11-02 18:38:41 +0530

    :zap: update init constants

--- src/pkgviz/__init__.py
+++ src/pkgviz/__init__.py
@@ -5,8 +5,8 @@ Description:
     Framework to visualise python packages.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.0.0"
 __author__ = "AjinkyaIndulkar <26824103+AjinkyaIndulkar@users.noreply.github.com>"
 __copyright__ = f"2022, {__author__}"
-__url__ = ""
+__url__ = "https://github.com/AjinkyaIndulkar/pkgviz-python"
 __docs__ = "Framework to visualise python packages."
