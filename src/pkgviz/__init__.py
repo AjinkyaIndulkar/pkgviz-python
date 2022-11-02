20a5c72dcb7b3325efdfe83b6f3951d462653218
Ajinkya Indulkar <26824103+AjinkyaIndulkar@users.noreply.github.com>
2022-11-02 15:49:10 +0530

    :tada: init package

---
+++ src/pkgviz/__init__.py
@@ -0,0 +1,12 @@
+"""
+Package Visualiser.
+
+Description:
+    Framework to visualise python packages.
+"""
+
+__version__ = "0.1.0"
+__author__ = "AjinkyaIndulkar <26824103+AjinkyaIndulkar@users.noreply.github.com>"
+__copyright__ = f"2022, {__author__}"
+__url__ = ""
+__docs__ = "Framework to visualise python packages."
