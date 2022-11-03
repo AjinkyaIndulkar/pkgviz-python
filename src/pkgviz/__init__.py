8c8bb75601d9b89bea7e9c84c380fe10f8251c1f
Ajinkya Indulkar <26824103+AjinkyaIndulkar@users.noreply.github.com>
2022-11-03 08:33:59 +0530

    :construction_worker: add publish workflow
    :heavy_plus_sign: add build dependency
    :wrench: update setup.cfg
    :fire: remove init variables

--- src/pkgviz/__init__.py
+++ src/pkgviz/__init__.py
@@ -6,7 +6,3 @@ Description:
 """
 
 __version__ = "0.0.0"
-__author__ = "AjinkyaIndulkar <26824103+AjinkyaIndulkar@users.noreply.github.com>"
-__copyright__ = f"2022, {__author__}"
-__url__ = "https://github.com/AjinkyaIndulkar/pkgviz-python"
-__docs__ = "Framework to visualise python packages."
